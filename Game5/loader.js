9baf8b49a3a62127457fc46bac1734ffeda920f2
gemhclarke <gemhclarke@gmail.com>
2025-01-07 12:58:41 +0000

    many changes: different images. misk folder for UI stuff

--- Game5/loader.js
+++
@@ -1,9 +0,0 @@
-    const preloadImages = (urls) => {
-        urls.forEach((url) => {
-            const img = new Image();
-            img.src = url;
-        });
-    };
-
-    // Call preloadImages with the list of image URLs
-    preloadImages(imageFiles);
